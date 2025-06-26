eedcda3466bf3717c8901b3bbef561fb5c5b8c7c
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2025-06-26 18:31:46 -0300

    Add files via upload
    Relógio Swing + Repetição + Repetição for + Soma Swing + Super Calculadora

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author PCGAMERCASA
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=0; cc<=3; cc++){//enquanto int = cont 0 cc for melhor que 3; cc conte mais 1
+            System.out.println("Cambalhotas");
+        }
+    }
+    
+}
