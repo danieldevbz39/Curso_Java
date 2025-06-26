5a5a4ba4658af972b867d31a33ef42e7b959ce1c
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2025-06-26 18:26:40 -0300

    Add files via upload
    Hora do sistema+ Leitor pensamento + números

---
+++ JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication28;
+
+/**
+ *
+ * @author PCGAMERCASA
+ */
+public class JavaApplication28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 2;
+        int n2 = 5;
+        int s = n1 + n2;
+        System.out.println("a soma é: " +s);
+    }
+    
+}
